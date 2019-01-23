572f0ecffbcdcb2a02f099d89ce02ab40b650d4e
APachon10 <Pachondestroy95@gmail.com>
2019-01-23 16:52:01 +0100

    readme y base de datos

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Proyecto2_M6
+Proyecto_Scrum
+
+Sprint 1
+
+Diseñar una Base de Datos para gestionar: Usuarios, grupos de usuarios, proyectos, especificaciones, sprints. (5 horas)
+Hay que utilizar JPA/Hibernate para el acceso a la base de datos remota. (3 horas)
+Los usuarios pueden ser de tipo: Developer (solo podrán visualizar los datos), Product Owner (puede visualizar los datos y añadir/modificar/eliminar especificaciones), Scrum Master (puede visualizar los datos, crear proyectos, crear sprints, temporizar especificaciones y sprints y asignar especificaciones a sprints) y Administrador de usuarios (puede crear usuarios). (1 hora)
+Cuando se inicie la aplicación ha de intentar de conectar a la Base de Datos remota e indicará la conexión con (ONLINE). (1 hora)
+Todas las ventanas internas (JInternalFrames) se podrán cerrar y redimensionar. (2 horas)
+Crear un JInternalFrames de Login. Aparece al ejecutar la aplicación. (1 hora)
+Al pulsar ENTER ha de intentar validar el usuario. (1 hora)
+Ha de tener un JInternalFrame para inscribir usuarios al programa. (4 horas)
+
+Indicaciones
+
+-Importar en BD MySQL el archivo que se encuentra dentro de main/resources/scrumprojectmanager.sql para hacer un volcado de información.
+
+-Ejecutar el programa en la clase "GestorDeProyectos".
+
+IMPORTANTE para que funcione correctamente la conexión a la BD Remota, comprueba dentro del archivo persistence.xml, dentro de main/resources/META-INF, en la etiqueta < property name="javax.persistence.jdbc.url" value="..." >, la información sea correcta.
+
+Lista de los usuarios:
+
+Usuario: pachon
+Contraseña: 123123
+Tipo de Usuario: null
---
+++ src/main/resources/scrumprojectmanager.sql
@@ -0,0 +1,251 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-01-2019 a las 16:33:25
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `scrumprojectmanager`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permisos`
+--
+
+CREATE TABLE `permisos` (
+  `permisoID` int(11) NOT NULL,
+  `permiso_name` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `projects`
+--
+
+CREATE TABLE `projects` (
+  `projectID` int(11) NOT NULL,
+  `project_name` varchar(20) NOT NULL,
+  `descripcion` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `specifications`
+--
+
+CREATE TABLE `specifications` (
+  `specID` int(11) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `projectID` int(11) NOT NULL,
+  `sprintID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sprints`
+--
+
+CREATE TABLE `sprints` (
+  `sprintID` int(11) NOT NULL,
+  `projectID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usergroups`
+--
+
+CREATE TABLE `usergroups` (
+  `groupID` int(11) NOT NULL,
+  `group_name` varchar(25) NOT NULL,
+  `projectID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `userID` int(11) NOT NULL,
+  `nickname` varchar(25) NOT NULL,
+  `complete_name` varchar(50) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `mail` varchar(30) NOT NULL,
+  `groupID` int(11) DEFAULT NULL,
+  `permisoID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`userID`, `nickname`, `complete_name`, `password`, `mail`, `groupID`, `permisoID`) VALUES
+(1, 'pachon', 'Alberto Pachon', '123123', 'pachon@gmail.com', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usersprojects`
+--
+
+CREATE TABLE `usersprojects` (
+  `userID` int(11) NOT NULL,
+  `projectID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  ADD PRIMARY KEY (`permisoID`);
+
+--
+-- Indices de la tabla `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`projectID`);
+
+--
+-- Indices de la tabla `specifications`
+--
+ALTER TABLE `specifications`
+  ADD PRIMARY KEY (`specID`),
+  ADD KEY `foreign_key_projectID` (`projectID`),
+  ADD KEY `foreign_key_sprintID` (`sprintID`);
+
+--
+-- Indices de la tabla `sprints`
+--
+ALTER TABLE `sprints`
+  ADD PRIMARY KEY (`sprintID`),
+  ADD KEY `foreign_key_projectID` (`projectID`);
+
+--
+-- Indices de la tabla `usergroups`
+--
+ALTER TABLE `usergroups`
+  ADD PRIMARY KEY (`groupID`),
+  ADD KEY `foreign_key_projectID` (`projectID`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userID`),
+  ADD KEY `foreign_key_groupID` (`groupID`),
+  ADD KEY `foreign_key_permisoID` (`permisoID`);
+
+--
+-- Indices de la tabla `usersprojects`
+--
+ALTER TABLE `usersprojects`
+  ADD KEY `foreign_key_userID` (`userID`),
+  ADD KEY `foreign_key_projectID` (`projectID`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  MODIFY `permisoID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `projectID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `specifications`
+--
+ALTER TABLE `specifications`
+  MODIFY `specID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `sprints`
+--
+ALTER TABLE `sprints`
+  MODIFY `sprintID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usergroups`
+--
+ALTER TABLE `usergroups`
+  MODIFY `groupID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `specifications`
+--
+ALTER TABLE `specifications`
+  ADD CONSTRAINT `specifications_ibfk_1` FOREIGN KEY (`projectID`) REFERENCES `projects` (`projectID`),
+  ADD CONSTRAINT `specifications_ibfk_2` FOREIGN KEY (`sprintID`) REFERENCES `sprints` (`sprintID`);
+
+--
+-- Filtros para la tabla `sprints`
+--
+ALTER TABLE `sprints`
+  ADD CONSTRAINT `sprints_ibfk_1` FOREIGN KEY (`projectID`) REFERENCES `projects` (`projectID`);
+
+--
+-- Filtros para la tabla `usergroups`
+--
+ALTER TABLE `usergroups`
+  ADD CONSTRAINT `usergroups_ibfk_1` FOREIGN KEY (`projectID`) REFERENCES `projects` (`projectID`);
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`groupID`) REFERENCES `usergroups` (`groupID`),
+  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`permisoID`) REFERENCES `permisos` (`permisoID`);
+
+--
+-- Filtros para la tabla `usersprojects`
+--
+ALTER TABLE `usersprojects`
+  ADD CONSTRAINT `usersprojects_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`),
+  ADD CONSTRAINT `usersprojects_ibfk_2` FOREIGN KEY (`projectID`) REFERENCES `projects` (`projectID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
