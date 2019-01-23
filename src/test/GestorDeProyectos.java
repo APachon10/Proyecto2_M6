2ddddb50acbed6ea59a77770e973adc42faede75
APachon10 <Pachondestroy95@gmail.com>
2019-01-23 17:26:17 +0100

    v

--- src/test/GestorDeProyectos.java
+++ src/test/GestorDeProyectos.java
@@ -210,15 +210,12 @@ public class GestorDeProyectos extends JFrame {
 			@Override
 			public void keyTyped(KeyEvent e) {
 				// TODO Auto-generated method stub
-
 			}
-
 			@Override
 			public void keyReleased(KeyEvent e) {
 				// TODO Auto-generated method stub
 
 			}
-
 			@Override
 			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
