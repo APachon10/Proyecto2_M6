4b849f78bc38112932123020dd242198b27ab971
APachon10 <Pachondestroy95@gmail.com>
2019-01-25 18:49:41 +0100

    Segundo Commit

--- src/vistas/VentanaProyecto.java
+++ src/vistas/VentanaProyecto.java
@@ -8,10 +8,17 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
 
 public class VentanaProyecto extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -25,21 +32,77 @@ public class VentanaProyecto extends JFrame {
 		});
 	}
 	public VentanaProyecto() {
+		setTitle("Nuevo Proyecto");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		
+		JLabel lblNombreProyecto = new JLabel("Nombre Proyecto");
+		
+		JLabel lblDescripcion = new JLabel("Descripcion");
+		
+		JLabel lblScrumMaster = new JLabel("Scrum Master");
+		
+		JLabel lblProductoOwner = new JLabel("Producto Owner");
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JTextArea textArea = new JTextArea();
+		
+		JComboBox comboBox = new JComboBox();
+		
+		JComboBox comboBox_1 = new JComboBox();
+		
+		JButton btnAadir = new JButton("A\u00F1adir");
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 424, Short.MAX_VALUE)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(28)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNombreProyecto)
+						.addComponent(lblDescripcion)
+						.addComponent(lblScrumMaster)
+						.addComponent(lblProductoOwner))
+					.addGap(26)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(btnAadir)
+							.addContainerGap())
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(comboBox_1, 0, 237, Short.MAX_VALUE)
+								.addComponent(textArea, GroupLayout.DEFAULT_SIZE, 237, Short.MAX_VALUE)
+								.addComponent(comboBox, 0, 237, Short.MAX_VALUE)
+								.addComponent(textField, GroupLayout.DEFAULT_SIZE, 237, Short.MAX_VALUE))
+							.addGap(50))))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 252, Short.MAX_VALUE)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(32)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNombreProyecto)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblDescripcion)
+						.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 56, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED, 6, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblScrumMaster)
+						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblProductoOwner)
+						.addComponent(comboBox_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addComponent(btnAadir)
+					.addGap(7))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
-
 }
