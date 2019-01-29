954884b62f2522b009957205223e5a9e631cbd9f
Darius <dariussb8@gmail.com>
2019-01-29 21:22:11 +0100

    Sprint 2

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
 Proyecto_Scrum
 
-Sprint 1
-
-Diseñar una Base de Datos para gestionar: Usuarios, grupos de usuarios, proyectos, especificaciones, sprints. (5 horas)
-Hay que utilizar JPA/Hibernate para el acceso a la base de datos remota. (3 horas)
-Los usuarios pueden ser de tipo: Developer (solo podrán visualizar los datos), Product Owner (puede visualizar los datos y añadir/modificar/eliminar especificaciones), Scrum Master (puede visualizar los datos, crear proyectos, crear sprints, temporizar especificaciones y sprints y asignar especificaciones a sprints) y Administrador de usuarios (puede crear usuarios). (1 hora)
-Cuando se inicie la aplicación ha de intentar de conectar a la Base de Datos remota e indicará la conexión con (ONLINE). (1 hora)
-Todas las ventanas internas (JInternalFrames) se podrán cerrar y redimensionar. (2 horas)
-Crear un JInternalFrames de Login. Aparece al ejecutar la aplicación. (1 hora)
-Al pulsar ENTER ha de intentar validar el usuario. (1 hora)
-Ha de tener un JInternalFrame para inscribir usuarios al programa. (4 horas)
-
 Indicaciones
 
 -Importar en BD MySQL el archivo que se encuentra dentro de main/resources/scrumprojectmanager.sql para hacer un volcado de información.
@@ -23,4 +12,19 @@ Lista de los usuarios:
 
 Usuario: pachon
 Contraseña: 123123
-Tipo de Usuario: null
+Tipo de Usuario: 1.Developer
+
+Usuario: vsalas
+Contraseña: 123123
+Tipo de Usuario: 2.Administrador
+
+Usuario: bsoubouti
+Contraseña: 123123
+Tipo de Usuario: 3.Scrum Manager
+
+Usuario: rcarballo
+Contraseña: 123123
+Tipo de Usuario: 4.Product Owner
+
+
+
--- src/main/vistas/CreateUser.java
+++ src/main/vistas/CreateUser.java
@@ -6,14 +6,21 @@ import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.awt.event.ActionEvent;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import main.interfaces.IUser;
@@ -23,15 +30,15 @@ import javax.swing.JComboBox;
 
 public class CreateUser {
 
-	private JTextField textField;
-	private JTextField textField_1;
-	private JPasswordField textField_2;
-	private JPasswordField textField_3;
-	private JTextField textField_4;
+	private JTextField txtName;
+	private JTextField txtLogin;
+	private JPasswordField txtPassword;
+	private JPasswordField txtRepeatPassword;
+	private JTextField txtMail;
 	private PrincipalFrame pf;
+	private boolean emailOk;
 	private IUser iuser;
-	
-	
+
 	public CreateUser(IUser iuser, PrincipalFrame pf) {
 		this.iuser = iuser;
 		this.pf = pf;
@@ -60,21 +67,21 @@ public class CreateUser {
 		
 		JLabel lblMail = new JLabel("Mail:");
 		
-		textField = new JTextField();
-		textField.setColumns(10);
+		txtName = new JTextField();
+		txtName.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setEditable(false);
-		textField_1.setColumns(10);
+		txtLogin = new JTextField();
+		txtLogin.setEditable(false);
+		txtLogin.setColumns(10);
 		
-		textField_2 = new JPasswordField();
-		textField_2.setColumns(10);
+		txtPassword = new JPasswordField();
+		txtPassword.setColumns(10);
 		
-		textField_3 = new JPasswordField();
-		textField_3.setColumns(10);
+		txtRepeatPassword = new JPasswordField();
+		txtRepeatPassword.setColumns(10);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
+		txtMail = new JTextField();
+		txtMail.setColumns(10);
 		
 		JButton btnGenerarPassword = new JButton("Generar Password");
 		btnGenerarPassword.addActionListener(new ActionListener() {
@@ -87,17 +94,116 @@ public class CreateUser {
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Seleccione un perfil de usuario", "Product Owner", "Scrum Master", "Developer", "Administrator"}));
 		comboBox.setFont(new Font("Tahoma", Font.BOLD, 11));
 		
+		// generate login
+				txtName.getDocument().addDocumentListener(new DocumentListener() {
+					
+					@Override
+					public void removeUpdate(DocumentEvent e) {
+						validation();
+					}
+					
+					@Override
+					public void insertUpdate(DocumentEvent e) {
+						validation();
+					}
+					
+					@Override
+					public void changedUpdate(DocumentEvent e) {
+						validation();
+					}
+					
+					public void validation() {
+						txtName.addFocusListener(new FocusListener() {
+
+							@Override
+							public void focusLost(FocusEvent e) {
+								String nombre = txtName.getText();
+								String[] arrayNombre = nombre.split(" ");
+								if (arrayNombre.length == 1) {
+									JOptionPane.showMessageDialog(null, "Formato del nombre incorrecto.", "Error",
+											JOptionPane.ERROR_MESSAGE);
+								}
+								txtLogin.setText(arrayNombre[0].substring(0, 1) + arrayNombre[1]);
+							}
+
+							@Override
+							public void focusGained(FocusEvent e) {
+								txtLogin.setText("");
+							}
+						});
+					}
+				});
+				
+				txtMail.addFocusListener(new FocusListener() {
+
+					@Override
+					public void focusLost(FocusEvent e) {
+						if (getMail(txtMail.getText()) == false) {
+							JOptionPane.showMessageDialog(null, "El formato del email incorrecto.", "Error",
+									JOptionPane.ERROR_MESSAGE);
+							txtMail.setText("");
+						}
+					}
+
+					private boolean getMail(String text) {
+						Pattern pattern = Pattern.compile(
+								"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+						String email = text;
+						Matcher mather = pattern.matcher(email);
+
+						if (!text.equals("")) {
+							if (mather.find() == true) {
+								emailOk = true;
+							} else {
+								emailOk = false;
+							}
+						}
+						return emailOk;
+					}
+
+					@Override
+					public void focusGained(FocusEvent e) {
+
+					}
+				});
+		
 		JButton btnGuardar = new JButton("Guardar");
 		btnGuardar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Users user=new Users();
-				user.setComplete_name(textField.getText());
-				user.setNickname("frita");//textField_1.getText()
-				user.setPassword(String.valueOf(textField_2.getPassword()));
-				user.setMail(textField_4.getText());
-				user.setPermiso_id(idPermiso(comboBox));
-				iuser.añadirUsuario(user);
+				if (txtName.getText().equals("") || txtPassword.getText().equals("")
+						|| txtRepeatPassword.getText().equals("") || txtMail.getText().equals("")
+						|| comboBox.getSelectedIndex() == 0) {
+					JOptionPane.showMessageDialog(null, "Faltan campos por rellenar.", "Error",
+							JOptionPane.ERROR_MESSAGE);
+				}
+				if (!txtName.getText().equals("") || !txtPassword.getText().equals("")
+						|| !txtRepeatPassword.getText().equals("") || !txtMail.getText().equals("")
+						|| comboBox.getSelectedIndex() != 0) {
+					if (txtPassword.getText().equals(txtRepeatPassword.getText())) {
+						//lblValidar.setText("");
+						//if (getMail(txtMail.getText()) == true) {
+							Users user=new Users();
+							user.setComplete_name(txtName.getText());
+							user.setNickname(txtLogin.getText());
+							user.setPassword(String.valueOf(txtPassword.getPassword()));
+							user.setMail(txtMail.getText());
+							user.setPermiso_id(idPermiso(comboBox));
+							iuser.añadirUsuario(user);
+							txtName.setText("");
+							txtLogin.setText("");
+							txtPassword.setText("");
+							txtRepeatPassword.setText("");
+							//chckbxMostrarPassword.setSelected(false);
+							txtMail.setText("");
+							comboBox.setSelectedIndex(0);
+						}
+					} else {
+						//lblValidar.setText("No coinciden");
+					}
+				//}
 			}
+			
+		
 
 			private Integer idPermiso(JComboBox comboBox) {
 				int id=0;
@@ -114,6 +220,8 @@ public class CreateUser {
 			}
 		});
 		
+		
+		
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
@@ -129,11 +237,11 @@ public class CreateUser {
 								.addComponent(lblMail))
 							.addGap(39)
 							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING, false)
-								.addComponent(textField_1, GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)
-								.addComponent(textField)
-								.addComponent(textField_2)
-								.addComponent(textField_3)
-								.addComponent(textField_4)))
+								.addComponent(txtLogin, GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)
+								.addComponent(txtName)
+								.addComponent(txtPassword)
+								.addComponent(txtRepeatPassword)
+								.addComponent(txtMail)))
 						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(43)
 					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING, false)
@@ -147,24 +255,24 @@ public class CreateUser {
 					.addGap(24)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblNombre)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(29)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblLoguinGenerado)
-						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtLogin, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(26)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblPassword)
-						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(btnGenerarPassword))
 					.addGap(38)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblRepitaPassword)
-						.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtRepeatPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(37)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblMail)
-						.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtMail, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(18)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
